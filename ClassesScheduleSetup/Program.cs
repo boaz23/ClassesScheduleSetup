c8a6df7a0929187807a2435b7c46e80224a940cb
boaz23 <boaz23mw@gmail.com>
2020-02-29 14:07:19 +0200

    Added code to main to allow running the algorithms

--- ClassesScheduleSetup/Program.cs
+++ ClassesScheduleSetup/Program.cs
@@ -10,7 +10,22 @@ namespace ClassesScheduleSetup
         static void Main()
         {
             CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("he-IL");
-            IEnumerable<Course> courses = Semesters.SemesterC.Courses;
+            IEnumerable<ClassSchedule> schedules = BuildSchedule(Semesters.SemesterC, false);
+        }
+
+        private static IEnumerable<ClassSchedule> BuildSchedule(Semester semester, bool takePracticeClassFromAllGroups)
+        {
+            ClassScheduleSetupAlgorithm algorithm;
+            if (takePracticeClassFromAllGroups)
+            {
+                algorithm = new PracticeClassFromAllGroupsSetupAlgorithm();
+            }
+            else
+            {
+                algorithm = new PracticeClassFromGroupOnlySetupAlgorithm();
+            }
+
+            return algorithm.CalculateSetup(semester);
         }
     }
 }
